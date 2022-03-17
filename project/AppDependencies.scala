a0a8ec3ae913673df34d3d38ce4d7f097f045e23
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2022-03-17 16:51:36 +0000

    Get tests running and passing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,17 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val compile = Seq(
+  private val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "0.58.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.6.0-play-27",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "4.2.0",
     "uk.gov.hmrc"       %% "play-language"                  % "4.12.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27"             % "0.49.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27"             % "0.49.0",
+    "com.google.inject" % "guice"                           % "5.0.1"
   )
 
-  val test = Seq(
+  private val test = Seq(
     "org.scalatest"           %% "scalatest"               % "3.2.7",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.7.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.7.0",
@@ -23,8 +24,8 @@ object AppDependencies {
     "org.mockito"             %% "mockito-scala"           % "1.16.0",
     "org.scalacheck"          %% "scalacheck"              % "1.15.3",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-27" % "0.49.0",
-    "com.vladsch.flexmark"    %  "flexmark-all"            % "0.35.10" // Required to stay at this version - see https://github.com/scalatest/scalatest/issues/1736
+    // "com.vladsch.flexmark"    %  "flexmark-all"            % "0.35.10" // Required to stay at this version - see https://github.com/scalatest/scalatest/issues/1736
   ).map(_ % "test, it")
 
-  def apply(): Seq[ModuleID] = compile ++ test
+  def apply(): Seq[ModuleID] = (compile ++ test).map(_.withSources())
 }
